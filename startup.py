def9519899ce6deec00cc11753bfeddd91dde561
gcvalderrama <gcvalderrama@hotmail.com>
2025-01-05 20:50:23 +0100

    fix

--- startup.py
+++ startup.py
@@ -1,5 +1,7 @@
 from src.components.TerminalComponent import TerminalComponent
 import logging
+import configparser
+import os
 
 logging.basicConfig(
     level=logging.INFO,
@@ -9,7 +11,21 @@ logging.basicConfig(
 
 LOGGER = logging.getLogger(__name__)
 
+def load_init():
+    LOGGER.info("Loading environment variables from startup.ini")
+    if not os.path.exists('startup.ini'):
+        raise ValueError("startup.ini file not found")
+    config = configparser.ConfigParser()
+    config.read('startup.ini')
+    
+    if 'DEFAULT' in config:
+        for key, value in config['DEFAULT'].items():
+            key = key.upper()
+            os.environ[key] = value
+            print(f"Setting {key}")
+
 if __name__ == "__main__":
     LOGGER.info("Starting the application")
+    load_init()
     component = TerminalComponent()
     component.run()

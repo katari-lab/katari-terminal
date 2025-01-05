6a35f368647ac1bfce464923133229b325659fb1
gcvalderrama <gcvalderrama@hotmail.com>
2025-01-05 21:14:18 +0100

    fix

--- src/gateways/TerminalGateway.py
+++ src/gateways/TerminalGateway.py
@@ -1,4 +1,7 @@
 import subprocess
+import logging
+
+LOGGER = logging.getLogger(__name__)
 
 class TerminalGateway:
 
@@ -7,7 +10,8 @@ class TerminalGateway:
 
     def execute_command(self, command):
         try:
-            result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+            LOGGER.info(f"Executing command: {command}")
+            result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)            
             print(result.stdout)
         except subprocess.CalledProcessError as e:            
             raise ValueError(f"Error executing command: {command} : {e.stderr}") from e
